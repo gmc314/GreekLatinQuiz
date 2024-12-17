407835ce65093cf646cdd244802238d22ca202f5
gmc314 <grant.cheng1@gmail.com>
2024-12-17 15:09:24 -0500

    add question element

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const engl_to_latin = [["water", "aqua, aquae, f"], ["to walk", "ambulo, ambulare"], ["king", "rex, regis, m"]]
 
+
+question_element = document.getElementById("question")
+text_container = document.getElementById("text-container")
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i+1));
@@ -7,3 +11,5 @@ function shuffleArray(array) {
     }
     return array
 }
+
+shuffled_words = shuffleArray(engl_to_latin)
